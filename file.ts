cc5257e0aba7c9d0ef56eb430cff6ac90d54ef07
Wael Fezai <fzwael@gmail.com>
2020-05-18 15:48:50 +0200

    Extract the latest version from Github :zap:

--- file.ts
+++ file.ts
@@ -1,6 +1,7 @@
 import { readJsonSync } from "https://deno.land/std/fs/read_json.ts";
 import { importsType } from "./types/importType.ts";
 import { dependencyType } from "./types/dependencyType.ts";
+import { addLatestVersions } from './version.ts'
 
 export async function readFile(filename: string) {
   console.debug("Trying to read file ", filename);
@@ -9,6 +10,7 @@ export async function readFile(filename: string) {
     console.log("Content ", fileContent);
     console.log("Content imports", fileContent.imports);
     console.log("Result is ", extractDependencyAndVersion(fileContent.imports));
+    addLatestVersions(extractDependencyAndVersion(fileContent.imports));
   } catch (err) {
     console.error("Cant read file !");
   }
@@ -20,8 +22,10 @@ function extractDependencyAndVersion(imports: object): dependencyType[] {
     // TODO find cleaner solution to extract version
     const substr = value.substring(value.indexOf("@") + 1);
     const exactVersion = substr.substring(0, substr.indexOf("/"));
+    // In case if there is an extra backslash at the end of the dependency name
+    const exactName = key.lastIndexOf('/') ? key.substring(0, key.lastIndexOf('/')) : key;
     result.push({
-      name: key,
+      name: exactName,
       url: value,
       version: exactVersion,
     });
---
+++ version.ts
@@ -0,0 +1,31 @@
+// We handle the methods to look for the latest versions
+import { dependencyType } from "./types/dependencyType.ts";
+import { soxa } from 'https://deno.land/x/soxa/mod.ts'
+
+export async function addLatestVersions(dependencies: dependencyType[]): Promise<dependencyType[]> {
+  let result;
+  const databaseResult = await soxa.get('https://raw.githubusercontent.com/denoland/deno_website2/master/database.json')
+    .catch((error) => {
+      console.error('Error getting the database.json file ', error);
+    });
+  const database = databaseResult.data;
+  result = await Promise.all(dependencies.map(async (dependency) => {
+    console.log('Dependency infos ', dependency.name);
+    if (database[dependency.name]?.type === 'github') {
+      const versionResult = await soxa.get(`https://api.github.com/repos/${database[dependency.name].owner}/${database[dependency.name].repo}/releases/latest`)
+        .catch((error) => {
+          console.error('Error getting the latest dependency ', error);
+        });
+      console.log('Got latest for ', dependency.name, '= ', versionResult.data.tag_name);
+      dependency.latest = versionResult.data.tag_name;
+      dependency.upToDate = versionResult.data.tag_name === dependency.version;
+      return dependency;
+
+    } else {
+      console.error('This type is not supported yet, please report this with dependency name = ', dependency.name);
+      return dependency;
+    }
+  }));
+  console.log('FINAL ', result);
+  return result;
+}
